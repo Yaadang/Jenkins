fb18a80d2422c00d018e433f10b1c8f18e23387e
Yaadang <107151903+Yaadang@users.noreply.github.com>
2023-04-26 16:45:45 -0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildApp(){
 def buildimage() {
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'ba245a61-f0b0-4e98-abaa-07a439f7b470', passwordVariable: 'PAS', usernameVariable: 'USER')]){
-        sh 'docker build -t yaadang/myrepo:jma-2.0 .'
+        sh 'docker build -t yaadang/myrepo:jma-2.1-test .'
         sh "echo $PAS | docker login -u $USER --password-stdin "
         sh 'docker push yaadang/myrepo:jma-2.1-test'
     }
